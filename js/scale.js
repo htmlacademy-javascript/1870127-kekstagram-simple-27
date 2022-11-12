c34246341dc315b8efbd3defb3a5f820aeb9dea9
KotovaAI <aikotova@mail.ru>
2022-11-12 13:54:24 +0300

    Помощь друга

--- js/scale.js
+++ js/scale.js
@@ -12,15 +12,15 @@ const SCALE_STEP = 25;
 const DEFAULT_SCALE = 100;
 
 const scaleImage = (value = DEFAULT_SCALE) => {
-  image.style.transform = `scale(${value / 100})`;
-  scaleInput.value = `${value}%`;
+ image.style.transform = `scale(${value / 100})`;
+ scaleInput.value = `${value}%`;
 };
 
 const onSmallerButtonClick = () => {
   const currentValue = parseInt(scaleInput.value, 10);
   let newValue = currentValue - SCALE_STEP;
   if (newValue < MIN_SCALE) {
-    newValue = MIN_SCALE;
+   newValue = MIN_SCALE;
   }
   scaleImage(newValue);
 };
@@ -29,13 +29,13 @@ const onBiggerButtonClick = () => {
   const currentValue = parseInt(scaleInput.value, 10);
   let newValue = currentValue + SCALE_STEP;
   if (newValue > MAX_SCALE) {
-    newValue = MAX_SCALE;
+   newValue = MAX_SCALE;
   }
   scaleImage(newValue);
 };
 
 const resetScale = () => {
-    scaleImage(DEFAULT_SCALE);
+ scaleImage(DEFAULT_SCALE);
 };
 
 smallerButton.addEventListener('click', onSmallerButtonClick);
